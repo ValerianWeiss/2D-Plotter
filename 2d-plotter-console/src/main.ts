d221456c0756f49b70e0b0505d744ae9085a1e07
Valerian <valerian97@web.de>
2021-02-09 13:01:00 +0100

    removed vuex

--- 2d-plotter-console/src/main.ts
+++ 2d-plotter-console/src/main.ts
@@ -1,6 +1,5 @@
 import Vue from 'vue';
 import App from './App.vue';
-import store from './store';
 import { Logger, LogLevel, LogMessageId } from './services/LogService';
 
 Vue.config.productionTip = false;
@@ -8,6 +7,5 @@ Logger.init(LogLevel.DEBUG, './2d-plotter-console.log');
 Logger.info('*** Started 2d-plotter-console ***', LogMessageId.CO_STARTED);
 
 new Vue({
-  store,
   render: h => h(App)
 }).$mount('#app');
